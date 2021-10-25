fae4112ef33a37942664663f354a2d18dae680a0
aek <konan.kouame@soprasteria.com>
2021-10-25 22:22:24 +0200

    add interceptor

---
+++ web/src/main/java/com/demo/aws/configuration/WebConfiguration.java
@@ -0,0 +1,21 @@
+package com.demo.aws.configuration;
+
+import com.demo.aws.filter.TenantInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    private final TenantInterceptor tenantInterceptor;
+
+    public WebConfiguration(TenantInterceptor tenantInterceptor) {
+        this.tenantInterceptor = tenantInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addWebRequestInterceptor(tenantInterceptor);
+    }
+}
